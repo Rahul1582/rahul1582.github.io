8f99258ad83c4717d02b0d9ce1da5999ae85ee26
Rahul1582 <rahulkpatro@gmail.com>
2020-05-16 18:04:21 +0530

    updated errors

--- js/main.js
+++ js/main.js
@@ -438,7 +438,7 @@ class TypeWriter {
   // Init App
   function init() {
     const txtElement = document.querySelector(".txt-type");
-    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    // const words = JSON.parse(txtElement.getAttribute("data-words"));
     const wait = txtElement.getAttribute("data-wait");
     // Init TypeWriter
     new TypeWriter(txtElement, words, wait);
