c321b3c8d127e7f401ab317166749135f88dcfea
Rahul1582 <rahulkpatro@gmail.com>
2020-05-17 00:50:37 +0530

    removed errors

--- js/main.js
+++ js/main.js
@@ -444,3 +444,13 @@ class TypeWriter {
     new TypeWriter(txtElement, words, wait);
   }
   
+
+// Preloader
+
+$(window).on('load', function() { // makes sure the whole site is loaded 
+    $('#status').fadeOut(4000); // will first fade out the loading animation 
+    $('#preloader').delay(4000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+    $('body').delay(2000).css({'overflow':'visible'});
+  })
+
+
